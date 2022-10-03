a5bb90e0881133cc1db7921f919602a2518fb07a
Otto Hajdu <ottohajdu123@gmail.com>
2022-10-03 16:54:55 +0200

    implémentation du mockdata avec PlatAddOrderView

--- LP-Besto/Model/Plat.swift
+++ LP-Besto/Model/Plat.swift
@@ -14,12 +14,13 @@ struct Plat : Identifiable {
     var id = UUID()
     var name : String
     var contenu : [Int] // Calories, Carbs, Proteins
+    var description : String
     var prix : Int
     
     static var mockData = [
-        Plat(name: "Steak cuit au Vin Rouge", contenu: [800, 22, 40], prix: 10),
-        Plat(name: "Tenders de Poulets", contenu: [400, 75, 12], prix: 12),
-        Plat(name: "Purée au Parmesan", contenu: [541, 32, 111], prix: 41),
+        Plat(name: "Steak cuit au Vin Rouge", contenu: [800, 22, 40], description: "Un steak cuit au Vin Rouge, traditionel de la Region" ,prix: 10),
+        Plat(name: "Tenders de Poulets", contenu: [400, 75, 12], description: "Des tenders recette secrete de notre restaurant",  prix: 12),
+        Plat(name: "Purée au Parmesan", contenu: [541, 32, 111], description: "Une purée faite avec amour, un plat très bon pour l'estomac",  prix: 41),
     ]
     
 }
