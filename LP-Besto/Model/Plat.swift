2d575986d7954e83158f33b447fd79dca054ae81
Otto Hajdu <ottohajdu123@gmail.com>
2022-10-03 15:40:31 +0200

    Ajout des groupes + model Plat

---
+++ LP-Besto/Model/Plat.swift
@@ -0,0 +1,18 @@
+//
+//  Plat.swift
+//  LP-Besto
+//
+//  Created by hajdu otto on 03/10/2022.
+//
+
+import Foundation
+
+// Un plat a un nom, le "contenu" alimentaire, un prix
+
+struct Plat {
+    
+    var name : String
+    var contenu : [Int]
+    var prix : Int
+    
+}
---
+++ LP-Besto/View/PlatAddOrderView.swift
@@ -0,0 +1,20 @@
+//
+//  PlatAddOrderView.swift
+//  LP-Besto
+//
+//  Created by hajdu otto on 03/10/2022.
+//
+
+import SwiftUI
+
+struct PlatAddOrderView: View {
+    var body: some View {
+        Text("gaming")
+    }
+}
+
+struct PlatAddOrderView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlatAddOrderView()
+    }
+}
