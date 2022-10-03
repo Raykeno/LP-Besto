b0b41d99ea45a5f12cc99965474800c7ea137276
Otto Hajdu <ottohajdu123@gmail.com>
2022-10-03 15:18:29 +0200

    Initial Commit

---
+++ LP-Besto/LP_BestoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LP_BestoApp.swift
+//  LP-Besto
+//
+//  Created by hajdu otto on 03/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct LP_BestoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
